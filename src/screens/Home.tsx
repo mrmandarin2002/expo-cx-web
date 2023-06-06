359f05a8293455147733354f6c21b9e7884fdceb
Derek Ma <derekk.ma@mail.utoronto.ca>
2023-06-06 16:42:27 -0400

    og

---
+++ src/screens/Home.tsx
@@ -0,0 +1,11 @@
+import LinkButton from 'src/components/LinkButton'
+import { Stack } from 'expo-router'
+import { Text } from "react-native";
+import styled from 'styled-components/native'
+
+export default function HomeScreen() {
+  return (
+    <Text> hello </Text>
+  )
+}
+
